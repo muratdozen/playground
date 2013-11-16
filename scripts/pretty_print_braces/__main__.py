b0b42110d26e6877236bfa174e1cffedb2e31a88
muratdozen <muratdozen@gmail.com>
2013-11-16 18:34:59 +0200

    added program description and fixed nonrecursive mode

--- scripts/pretty_print_braces/__main__.py
+++ scripts/pretty_print_braces/__main__.py
@@ -1,5 +1,59 @@
 __author__ = 'Murat Derya Ozen'
 
+PROGRAM_DESCRIPTION = """
+Spot the ugliest files in a code repository.
+Given a path to a file or a directory, this program parses source files to extract,
+pretty print and calculate the number of curly brackets, i.e braces.
+
+*** Samples usages ***
+
+> python pretty_print_braces file --path /root/repo/svn/trunk/SomeJavaClass.java
+
+Assuming SomeJavaClass.java is a simple Java source file containing the following code:
+    public class SomeJavaClass {
+        public static void main(String[] args) {
+            if (args != null && args.length > 2) {
+                System.out.println("Hello");
+            }
+        }
+    }
+
+The program would output:
+/root/repo/svn/trunk/SomeJavaClass.java
+{
+    {
+        {
+        }
+    }
+}
+Depths: [1, 1, 1]
+Max depth: 3
+
+The output starts with the file name, followed by the braces in the file pretty-printed,
+followed by the number of braces at each level, finally followed by the maximum depth.
+This means that the source file contains at most 3 levels of nested braces.
+Furthermore; we see that at there is 1 opening and closing brace for each level
+(i.e 1 for the class declaration, 1 for the static main method declaration and 1 for the
+if statement).
+
+
+> python pretty_print_braces file --path /root/repo/svn/trunk/SomeJavaClass.java -q
+
+Adding '-q' or '--quiet' switches to quite mode where pretty-printing of the braces are not
+included in the output.
+
+
+> python pretty_print_braces dir -p /root/repo/svn/trunk
+
+This command process the whole directory (recursively) as opposed to a single file. It processes each
+source file and prints the ugliest 5. Ugliness is defined by having the most depth of nested braces.
+
+> python pretty_print_braces dir -p /root/repo/svn/trunk --nonrecursive --extensions=cpp,c,h -q -n 3
+
+Process the source files with extensions cpp, c or h in the directory at /root/repo/svn/trunk. Output the
+ugliest 3 source files in quiet mode. --nonrecursive indicates that subdirectories of trunk will not be traversed.
+"""
+
 import argparse
 import heapq
 import re
@@ -36,7 +90,7 @@ def extract_braces(source_code, filename, quiet):
             if not quiet:
                 braces.append((INDENT * current_depth) + '}')
     max_depth = len(depths) - 1
-    return max_depth, depths, LINE_SEPARATOR.join(braces), filename
+    return max_depth, depths[1:], LINE_SEPARATOR.join(braces), filename
 
 
 def analyze_file(filename, quiet):
@@ -87,14 +141,13 @@ def walk_through_directory(rootpath, recursive, n, file_extensions, quiet):
         for dirpath, _, filenames in os.walk(rootpath):
             process_directory(dirpath, filenames)
     else:
-        for filenames in os.listdir(rootpath):
-            process_directory(rootpath, filenames)
+        process_directory(rootpath, os.listdir(rootpath))
     return h
 
 
 def get_argparser():
     """ Defines and returns the ArgumentParser for this program. """
-    parser = argparse.ArgumentParser(description='TODO') #TODO
+    parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
     sub_parsers = parser.add_subparsers()
 
     file_sub_parser = sub_parsers.add_parser('file')
@@ -120,8 +173,7 @@ def get_argparser():
                                      + 'argument can take a single extension or a comma separated list. '
                                      + 'Default is \'java,cpp,c,h\'.')
     dir_sub_parser.add_argument('-n', type=int, default=5,
-                                help='Resulting output includes the ugliest \'n\' files. Files are '
-                                     + 'presented in decreasing order of ugliness. Ugliness is defined by '
+                                help='Resulting output includes the ugliest \'n\' files. Ugliness is defined by '
                                      + 'the depth of braces. For instance, the Java class defined as '
                                      + '\'class A { void method() { if(true) {return;} } }\' would have a '
                                      + ' depth of 3 as it\'s curly braces go 3 levels deep. By default n=5.')
